f47e7059e3165ea96db6ff648b0a0ca5a98ed28d
Håkon Erichsen <hakon.erichsen@gmail.com>
2017-07-10 14:05:08 -0700

    Fix tiny bug with retries

--- src/index.ts
+++ src/index.ts
@@ -184,7 +184,7 @@ export class RequestQueue {
       this.dequeue(req)
       req.onDone(response)
     }).catch((e: any) => {
-      const retryCount = req.maxRetries !== undefined ? req.maxTries : this.retries
+      const retryCount = req.maxRetries !== null ? req.maxRetries : this.retries
       if (req.sendAttempts < retryCount) {
         req.status = RequestStatus.PENDING
         // Re-send request
